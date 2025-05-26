f9c9658e0c0e934b9076efef16b716fc4f491358
STT196 <109481005+STT196@users.noreply.github.com.>
2025-05-26 10:21:03 +0000

    Firebase and signup function

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,8 +48,10 @@ dependencies {
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
     implementation(platform("com.google.firebase:firebase-bom:33.14.0"))
+    implementation(platform("com.google.firebase:firebase-firestore"))
 }
 dependencies {
+    implementation(libs.firebase.firestore)
     implementation(libs.firebase.auth)
     implementation(libs.firebase.common)
     implementation("com.github.bumptech.glide:glide:4.15.1")
